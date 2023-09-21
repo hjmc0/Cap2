fc33691fda9db9a42212d3afd5201b79e88e935b
shurui99 <shurui9910@gmail.com>
2023-09-21 15:02:34 +0800

    Updated

---
+++ cap2/src/main/java/com/cecil/account/ApplicationManager.java
@@ -0,0 +1,34 @@
+package com.cecil.account;
+
+public class ApplicationManager {
+
+    public void execute(String operation){
+        switch(operation) {
+            case "add":
+              
+              break;
+            case "view":
+              // code block
+              break;
+            case "modify":
+              
+              break;
+            case "close":
+              
+              break;
+            case "deposit":
+              
+              break;
+            case "withdraw":
+              
+              break;
+            case "viewbal":
+              
+              break;
+            default:
+              // code block
+          }
+
+    }
+}
+
