95bee67208d9c98fa8cb2e6d02171fe19d7a7cbd
zhengxiangxp <54161150+zhengxiangxp@users.noreply.github.com>
2023-09-21 11:32:03 +0800

    AddAccount v1.01

--- cap2/src/main/java/com/cecil/account/AddAccount.java
+++ cap2/src/main/java/com/cecil/account/AddAccount.java
@@ -1,4 +1,4 @@
-package com.cecil;
+package com.cecil.account;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,16 +7,16 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class AddAccount {
-        Scanner scan = new Scanner(System.in);
-        static boolean more = true;
-        static int aid;
-        static String aname;
-        static int bal;
         
         public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+               
                 try {
                         Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "john", "pass");
                         Statement stmt = conn.createStatement();
+
+                        System.out.println("Enter Account Number: ");
+                        int aid = scan.nextLine();
 
 
                         while (more) {
@@ -25,7 +25,14 @@ public class AddAccount {
                                                 + "'"+ aid+ "'"
                                                 + "'"+ aname + "'"
                                                 + "'"+ bal +"')"; 
-                                
+
+                                                int rec = stmt.executeUpdate(addAcc);
+                                                System.out.println(rec + " records inserted");
+                                                System.out.print("Enter another record? (y/n): ");
+                                                String choice = scan.next();
+                                                if (choice.equalsIgnoreCase("n")){
+                                                    more = false;
+                                                }
                         }
 
                 } catch (SQLException e) {
