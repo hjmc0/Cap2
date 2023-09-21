6eaa19917819e0906225de5ca5ad933c1c41c5f5
hjmc0 <hjmc0@outlook.com>
2023-09-21 16:11:56 +0800

    Updated connections

--- cap2/src/main/java/com/cecil/account/AddAccount.java
+++ cap2/src/main/java/com/cecil/account/AddAccount.java
@@ -1,10 +1,10 @@
 package com.cecil.account;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
+
+import connection.Connections;
 
 public class AddAccount {
         public static void main(String[] args) {
@@ -12,11 +12,7 @@ public class AddAccount {
                 boolean more = true;
 
                 try {
-                        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "john",
-                                        "pass");
-                        System.out.println("Connection Established");
-                        assert conn != null : "No connection";
-                        Statement stmt = conn.createStatement();
+                        Statement stmt = Connections.openConn().createStatement();
 
                         while (more) {
                                 System.out.print("Enter New Account Number: ");
