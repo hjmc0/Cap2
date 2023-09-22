bc349110ac50ad26af847f505867bf9e7bd3c7ba
ljinan <87352325+ljinan@users.noreply.github.com>
2023-09-22 10:58:17 +0800

    test file

---
+++ cap2/src/main/java/com/cecil/testing/InsertTimeStamp.java
@@ -0,0 +1,28 @@
+package com.cecil.testing;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import connection.Connections;
+
+public class InsertTimeStamp {
+ 
+    
+    public static void main(String[] args) {
+        Connection conn  = Connections.openConn();
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement("insert into testdate values(?)");
+            LocalDateTime ld = LocalDateTime.now();
+            Timestamp ts = Timestamp.valueOf(ld);
+            stmt.setTimestamp(1, ts);
+            stmt.executeUpdate();
+            System.out.println("Rec inserted");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- cap2/src/main/java/connection/Connections.java
+++ cap2/src/main/java/connection/Connections.java
@@ -10,11 +10,11 @@ public class Connections {
     static public Connection openConn() {
         try {
             // conn depends on whether you're using mysql or oracledb
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cap2", "root", "dummy");
+            // conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cap2", "root", "dummy");
             // conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","root", "dummy");
-            // conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","cecil", "pass");
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl",
-                    "shurui99", "pass");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","cecil", "pass");
+            // conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl",
+                    // "shurui99", "pass");
             // conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl",
             //         "shurui99", "pass");
             assert conn != null : "Connection not successful";
