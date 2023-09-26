c5e170ccada8eb74bc3c7a513a7e7626a341cf6a
shurui99 <shurui9910@gmail.com>
2023-09-26 14:12:34 +0800

    Updated deposit and withdrawal

--- cap2/src/main/java/com/cecil/account/Deposit.java
+++ cap2/src/main/java/com/cecil/account/Deposit.java
@@ -12,110 +12,109 @@ import com.cecil.connection.Connections;
 
 public class Deposit {
 
-    static boolean aidMatcher = false;
     public static void depositAmt(int aid) {
 
         try {
             LocalDateTime dateTime = LocalDateTime.now();
             Timestamp ts = Timestamp.valueOf(dateTime);
 
+            String sql = "select aid, balance from account where aid  = ?";
+            PreparedStatement pstmt = Connections.openConn().prepareStatement(sql);
+            pstmt.setInt(1, aid);
+            ResultSet r = pstmt.executeQuery();
 
-            while (!aidMatcher) {
-                String sql = "select aid, balance from account where aid  = ?";
-                PreparedStatement pstmt = Connections.openConn().prepareStatement(sql);
-                pstmt.setInt(1, aid);
-                ResultSet r = pstmt.executeQuery();
+            int temp = 0;
+            double curBal = 0;
+            double tempBal = 0;
+            double newBal = 0;
+            int sure = 0;
+            String reset = "\u001B[0m"; // Reset color
+            String cyan = "\u001B[36m"; // Cyan
+            String red = "\u001B[31m"; // Red
+            String green = "\u001B[32m"; // Green
+            String bold = "\033[0;1m"; // Bold
+            String yellow = "\u001B[33m"; // Yellow
 
-                int temp = 0;
-                double curBal = 0;
-                double tempBal = 0;
-                double newBal = 0;
-                int sure = 0;
-                String reset = "\u001B[0m"; // Reset color
-                String cyan = "\u001B[36m";   // Cyan
-                String red = "\u001B[31m";   // Red
-                String green = "\u001B[32m"; // Green
-                String bold = "\033[0;1m"; // Bold
-                String yellow = "\u001B[33m"; // Yellow
+            // retrieving
+            while (r.next()) {
+                temp = r.getInt("aid");
+                curBal = r.getInt("balance");
+            }
 
-                // retrieving
-                while (r.next()) {
-                    temp = r.getInt("aid");
-                    curBal = r.getInt("balance");
-                }
+            if (temp == aid) {
 
-                if (temp == aid) {
-                    aidMatcher = true;
+                while (sure != 1) {
+                    System.out.println("--------------------" + cyan + "CURRENT BALANCE $"
+                            + String.format("%.2f", curBal) + reset + " --------------------");
 
-                    while (sure != 1) {
-                        System.out.println("--------------------"+ cyan +"CURRENT BALANCE $" + String.format("%.2f", curBal) + reset +" --------------------");
-                        
-                        Boolean valid = false;
-                        while (!valid){
-                            System.out.print("Enter Deposit Amount: $"); // Entry of deposit amount and calculation
-                            try {
-                                tempBal = Math.abs(Application.scan.nextDouble());
-                                Application.scan.nextLine();
-                                valid = true;
-                            } catch (InputMismatchException e) {
-                                Application.scan.nextLine();
-                                System.out.println(bold +red +"Invalid Deposit Amount. Please try again."+ reset); 
-                            }
-                            
-                        }
-                        sure = 0;
-
-                        while (sure != 1 && sure != 2) {
-                            System.out.println("-----------------------------------------------------------");
-                            System.out
-                                    .println("Continue to "+ bold + green + "DEPOSIT $" + String.format("%.2f", tempBal) + "\u001B[0m" + " into ACCOUNT No.: " + aid + " ?");
-                            System.out.println("1. To continue deposit");
-                            System.out.println("2. To change amount");
-                            System.out.print("Select : ");
-                            sure = Application.scan.nextInt();
+                    Boolean valid = false;
+                    while (!valid) {
+                        System.out.print("Enter Deposit Amount: $"); // Entry of deposit amount and calculation
+                        try {
+                            tempBal = Math.abs(Application.scan.nextDouble());
                             Application.scan.nextLine();
-                            if ((sure != 2) && (sure != 1)) {
-                                System.out.println(bold + red+"Invalid option"+ reset);
+                            valid = true;
+                        } catch (InputMismatchException e) {
+                            Application.scan.nextLine();
+                            System.out.println(bold + red + "Invalid Deposit Amount. Please try again." + reset);
+                        }
 
-                            }
+                    }
+                    sure = 0;
+
+                    while (sure != 1 && sure != 2) {
+                        System.out.println("-----------------------------------------------------------");
+                        System.out
+                                .println("Continue to " + bold + green + "DEPOSIT $" + String.format("%.2f", tempBal)
+                                        + "\u001B[0m" + " into ACCOUNT No.: " + aid + " ?");
+                        System.out.println("1. To continue deposit");
+                        System.out.println("2. To change amount");
+                        System.out.print("Select : ");
+                        sure = Application.scan.nextInt();
+                        Application.scan.nextLine();
+                        if ((sure != 2) && (sure != 1)) {
+                            System.out.println(bold + red + "Invalid option" + reset);
+
                         }
                     }
-                    newBal = tempBal + curBal;
-                    String sql2 = "select max(trans_id) from transaction";
-                    PreparedStatement pstmt1 = Connections.openConn().prepareStatement(sql2);
-                    ResultSet trans = pstmt1.executeQuery();
-                    trans.next();
-
-                    int nextTransId = trans.getInt(1) + 1;
-
-                    String deptSQL = "update Account set balance= ? where aid = ?";
-                    PreparedStatement pstmt2 = Connections.openConn().prepareStatement(deptSQL);
-                    pstmt2.setDouble(1, newBal);
-                    pstmt2.setInt(2, aid);
-                    pstmt2.execute();
-
-                    String deptSQLT = "insert into transaction(trans_id, trans_date, trans_type, aid, amount) values (?,?,?,?,?)";
-                    PreparedStatement pstmt3 = Connections.openConn().prepareStatement(deptSQLT);
-                    pstmt3.setInt(1, nextTransId);
-                    pstmt3.setTimestamp(2, ts);
-                    pstmt3.setString(3, "DEPOSIT");
-                    pstmt3.setInt(4, aid);
-                    pstmt3.setDouble(5, tempBal);
-                    pstmt3.execute();
-
-                    System.out.println("===============================================================");
-                    System.out.println("====================== "+ bold + cyan +"DEPOSIT COMPLETED "+ reset +"======================");
-                    System.out.println("ACCOUNT NUMBER            :     " + aid);
-                    System.out.println("ORIGINAL TOTAL BALANCE    :   $ " + String.format("%.2f", curBal));
-                    System.out.println("DEPOSITED AMOUNT          :"+green+" + $ " + String.format("%.2f", tempBal)+reset);
-                    System.out.println("NEW TOTAL BALANCE         :   $ "+ bold + String.format("%.2f", newBal)+reset);
-                    System.out.println("============================"+bold+yellow+"END"+reset+"================================");
-
-                } else {
-                    aidMatcher = true;
-                    System.out.println(red+"------------ Account ID does not exist, try again! ------------"+reset);
-                    System.out.println("     ");
                 }
+                newBal = tempBal + curBal;
+                String sql2 = "select max(trans_id) from transaction";
+                PreparedStatement pstmt1 = Connections.openConn().prepareStatement(sql2);
+                ResultSet trans = pstmt1.executeQuery();
+                trans.next();
+
+                int nextTransId = trans.getInt(1) + 1;
+
+                String deptSQL = "update Account set balance= ? where aid = ?";
+                PreparedStatement pstmt2 = Connections.openConn().prepareStatement(deptSQL);
+                pstmt2.setDouble(1, newBal);
+                pstmt2.setInt(2, aid);
+                pstmt2.execute();
+
+                String deptSQLT = "insert into transaction(trans_id, trans_date, trans_type, aid, amount) values (?,?,?,?,?)";
+                PreparedStatement pstmt3 = Connections.openConn().prepareStatement(deptSQLT);
+                pstmt3.setInt(1, nextTransId);
+                pstmt3.setTimestamp(2, ts);
+                pstmt3.setString(3, "DEPOSIT");
+                pstmt3.setInt(4, aid);
+                pstmt3.setDouble(5, tempBal);
+                pstmt3.execute();
+
+                System.out.println("===============================================================");
+                System.out.println("====================== " + bold + cyan + "DEPOSIT COMPLETED " + reset
+                        + "======================");
+                System.out.println("ACCOUNT NUMBER            :     " + aid);
+                System.out.println("ORIGINAL TOTAL BALANCE    :   $ " + String.format("%.2f", curBal));
+                System.out.println(
+                        "DEPOSITED AMOUNT          :" + green + " + $ " + String.format("%.2f", tempBal) + reset);
+                System.out.println("NEW TOTAL BALANCE         :   $ " + bold + String.format("%.2f", newBal) + reset);
+                System.out.println("============================" + bold + yellow + "END" + reset
+                        + "================================");
+
+            } else {
+                System.out.println(red + "------------ Account ID does not exist, try again! ------------" + reset);
+                System.out.println("     ");
             }
 
         } catch (SQLException e) {
--- cap2/src/main/java/com/cecil/account/Withdraw.java
+++ cap2/src/main/java/com/cecil/account/Withdraw.java
@@ -17,117 +17,115 @@ public class Withdraw {
             LocalDateTime dateTime = LocalDateTime.now();
             Timestamp ts = Timestamp.valueOf(dateTime);
 
-            boolean aidMatcher = false;
+            String sql = "select aid, balance from account where aid  = ?";
+            PreparedStatement pstmt = Connections.openConn().prepareStatement(sql);
+            pstmt.setInt(1, acctID);
+            ResultSet r = pstmt.executeQuery();
 
-            while (!aidMatcher) {
-                String sql = "select aid, balance from account where aid  = ?";
-                PreparedStatement pstmt = Connections.openConn().prepareStatement(sql);
-                pstmt.setInt(1, acctID);
-                ResultSet r = pstmt.executeQuery();
+            int temp = 0;
+            double curBal = 0;
+            double tempBal = 0;
+            double newBal = 0;
+            int sure = 0;
+            String reset = "\u001B[0m"; // Reset color
+            String cyan = "\u001B[36m"; // Cyan
+            String red = "\u001B[31m"; // Red
+            String bold = "\033[0;1m"; // Bold
+            String yellow = "\u001B[33m"; // Yellow
 
-                int temp = 0;
-                double curBal = 0;
-                double tempBal = 0;
-                double newBal = 0;
-                int sure = 0;
-                String reset = "\u001B[0m"; // Reset color
-                String cyan = "\u001B[36m";   // Cyan
-                String red = "\u001B[31m";   // Red
-                String bold = "\033[0;1m"; // Bold
-                String yellow = "\u001B[33m"; // Yellow
+            // retrieving
+            while (r.next()) {
+                temp = r.getInt("aid");
+                curBal = r.getInt("balance");
+            }
 
-                // retrieving
-                while (r.next()) {
-                    temp = r.getInt("aid");
-                    curBal = r.getInt("balance");
-                }
+            if (temp == acctID) {
 
-                if (temp == acctID) {
-                    aidMatcher = true;
+                while (sure != 1) {
+                    System.out.println("-------------------- " + cyan + "CURRENT BALANCE: $"
+                            + String.format("%.2f", curBal) + reset + " --------------------");
 
-                    while (sure != 1) {
-                        System.out.println("-------------------- "+ cyan +"CURRENT BALANCE: $" + String.format("%.2f", curBal) +reset +" --------------------");
+                    Boolean overBal = false;
+                    Boolean valid = false;
 
-                        Boolean overBal = false;
-                        Boolean valid = false;
-
-                        
-
-                            while (!valid && !overBal) {
-                                System.out.print("Enter Withdrawal Amount: $"); 
-                                try {
-                                    tempBal = Math.abs(Application.scan.nextDouble());
-                                    Application.scan.nextLine();
-
-                                    if (tempBal <= curBal) {
-                                    overBal = true;
-                                     valid = true;
-                                    } else {
-                                        System.out.println(red+"Withdrawal amount exceeds the Current Account Balance!");
-                                        System.out.println("Please try again."+ reset);
-                                    }
-                                   
-                                } catch (InputMismatchException e) {
-                                    Application.scan.nextLine();
-                                    System.out.println(red+ "Invalid Withdrawal Amount. Please try again."+ reset);
-                                }
-                            
-                            }
-                        
-                        sure = 0;
-
-                        while (sure != 1 && sure != 2) {
-                            System.out.println("-----------------------------------------------------------");
-                            System.out.println(
-                                    "Continue to "+bold+ red +"WITHDRAW $" + String.format("%.2f", tempBal) +reset+ " from ACCOUNT No.: " + acctID + " ?");
-                            System.out.println("1. To continue withdrawal");
-                            System.out.println("2. To change amount");
-                            System.out.print("Select : ");
-                            sure = Application.scan.nextInt();
+                    while (!valid && !overBal) {
+                        System.out.print("Enter Withdrawal Amount: $");
+                        try {
+                            tempBal = Math.abs(Application.scan.nextDouble());
                             Application.scan.nextLine();
-                            if ((sure != 1) && (sure != 2)) {
-                                System.out.println(red+"Invalid option"+reset);
 
+                            if (tempBal <= curBal) {
+                                overBal = true;
+                                valid = true;
+                            } else {
+                                System.out.println(red + "Withdrawal amount exceeds the Current Account Balance!");
+                                System.out.println("Please try again." + reset);
                             }
+
+                        } catch (InputMismatchException e) {
+                            Application.scan.nextLine();
+                            System.out.println(red + "Invalid Withdrawal Amount. Please try again." + reset);
+                        }
+
+                    }
+
+                    sure = 0;
+
+                    while (sure != 1 && sure != 2) {
+                        System.out.println("-----------------------------------------------------------");
+                        System.out.println(
+                                "Continue to " + bold + red + "WITHDRAW $" + String.format("%.2f", tempBal) + reset
+                                        + " from ACCOUNT No.: " + acctID + " ?");
+                        System.out.println("1. To continue withdrawal");
+                        System.out.println("2. To change amount");
+                        System.out.print("Select : ");
+                        sure = Application.scan.nextInt();
+                        Application.scan.nextLine();
+                        if ((sure != 1) && (sure != 2)) {
+                            System.out.println(red + "Invalid option" + reset);
+
                         }
                     }
-                    newBal = curBal - tempBal;
-
-                    String sql2 = "select max(trans_id) from transaction";
-                    PreparedStatement pstmt1 = Connections.openConn().prepareStatement(sql2);
-                    ResultSet trans = pstmt1.executeQuery();
-                    trans.next();
-
-                    int nextTransId = trans.getInt(1) + 1;
-
-                    String deptSQL = "update Account set balance= ? where aid = ?";
-                    PreparedStatement pstmt2 = Connections.openConn().prepareStatement(deptSQL);
-                    pstmt2.setDouble(1, newBal);
-                    pstmt2.setInt(2, acctID);
-                    pstmt2.execute();
-
-                    String deptSQLT = "insert into transaction(trans_id, trans_date, trans_type, aid, amount) values (?,?,?,?,?)";
-                    PreparedStatement pstmt3 = Connections.openConn().prepareStatement(deptSQLT);
-                    pstmt3.setInt(1, nextTransId);
-                    pstmt3.setTimestamp(2, ts);
-                    pstmt3.setString(3, "WITHDRAW");
-                    pstmt3.setInt(4, acctID);
-                    pstmt3.setDouble(5, tempBal);
-                    pstmt3.execute();
-
-                    System.out.println("===============================================================");
-                    System.out.println("====================== "+bold+cyan+"WITHDRAW COMPLETED"+reset+" =====================");
-                    System.out.println("ACCOUNT NUMBER            :     " + acctID);
-                    System.out.println("ORIGINAL TOTAL BALANCE    :   $ " + String.format("%.2f", curBal));
-                    System.out.println("WITHDRAW AMOUNT           :"+red+" - $ " + String.format("%.2f", tempBal) + reset);
-                    System.out.println("NEW TOTAL BALANCE         :   $ "+bold + String.format("%.2f", newBal)+reset);
-                    System.out.println("============================"+bold+yellow+"END"+reset+"================================");
-
-                } else {
-                    aidMatcher = false;
-                    System.out.println(red+"------------ Account ID does not exist, try again! ------------"+reset);
-                    System.out.println("     ");
                 }
+                newBal = curBal - tempBal;
+
+                String sql2 = "select max(trans_id) from transaction";
+                PreparedStatement pstmt1 = Connections.openConn().prepareStatement(sql2);
+                ResultSet trans = pstmt1.executeQuery();
+                trans.next();
+
+                int nextTransId = trans.getInt(1) + 1;
+
+                String deptSQL = "update Account set balance= ? where aid = ?";
+                PreparedStatement pstmt2 = Connections.openConn().prepareStatement(deptSQL);
+                pstmt2.setDouble(1, newBal);
+                pstmt2.setInt(2, acctID);
+                pstmt2.execute();
+
+                String deptSQLT = "insert into transaction(trans_id, trans_date, trans_type, aid, amount) values (?,?,?,?,?)";
+                PreparedStatement pstmt3 = Connections.openConn().prepareStatement(deptSQLT);
+                pstmt3.setInt(1, nextTransId);
+                pstmt3.setTimestamp(2, ts);
+                pstmt3.setString(3, "WITHDRAW");
+                pstmt3.setInt(4, acctID);
+                pstmt3.setDouble(5, tempBal);
+                pstmt3.execute();
+
+                System.out.println("===============================================================");
+                System.out.println("====================== " + bold + cyan + "WITHDRAW COMPLETED" + reset
+                        + " =====================");
+                System.out.println("ACCOUNT NUMBER            :     " + acctID);
+                System.out.println("ORIGINAL TOTAL BALANCE    :   $ " + String.format("%.2f", curBal));
+                System.out.println(
+                        "WITHDRAW AMOUNT           :" + red + " - $ " + String.format("%.2f", tempBal) + reset);
+                System.out.println("NEW TOTAL BALANCE         :   $ " + bold + String.format("%.2f", newBal) + reset);
+                System.out.println("============================" + bold + yellow + "END" + reset
+                        + "================================");
+
+            } else {
+
+                System.out.println(red + "------------ Account ID does not exist, try again! ------------" + reset);
+                System.out.println("     ");
             }
 
         } catch (SQLException e) {
