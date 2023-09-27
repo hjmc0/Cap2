b27f7543febfece622cbbd8cc2a019ebb6bff204
hjmc0 <hjmc0@outlook.com>
2023-09-27 11:28:11 +0800

    Update ViewAllAccounts.java

--- cap2/src/main/java/com/cecil/account/ViewAllAccounts.java
+++ cap2/src/main/java/com/cecil/account/ViewAllAccounts.java
@@ -16,11 +16,12 @@ public class ViewAllAccounts {
             String reset = "\u001B[0m"; // Reset color
             String red = "\u001B[31m"; // Red
             String green = "\u001B[32m"; // Green
-            System.out.printf("%s%-6s | %-15s | %-15s | %-10s %s%n", red, "Index.", "Account ID", "Holder Name",
-                    "Account Balance", reset);
+            System.out.printf("%s%-6s | %-10s | %-15s | %-25s | %-15s | %-15s | %10s %n", red,
+                    "Index.", "Account ID", "Name", "Email", "Phone Number", "Address", "Account Balance", reset);
             while (r.next()) {
-                System.out.printf("%s%-6s | %-15s | %-15s | $%10.2f  %s%n",
-                        green, cnt + ".", r.getInt("aid"), r.getString("aname"), r.getDouble("balance"), reset);
+                System.out.printf("%s%-6s | %-10s | %-15s | %-25s | %-15s | %-15s | $%10.2f  %s%n",
+                        green, cnt + ".", r.getInt("aid"), r.getString("aname"), r.getString("email"),
+                        r.getInt("phone"), r.getString("address"), r.getDouble("balance"), reset);
                 cnt += 1;
             }
 
